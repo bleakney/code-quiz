dae4106f2a501022a2b743e398c956f5723afba6
Bleakney Bissett <bleakneybissett@gmail.com>
2021-06-02 21:34:12 -0500

    set up quiz and add first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,17 @@ var timerEl = document.getElementById("timer");
 var currentScoreEl = document.getElementById("currentScore")
 var startBtnEl = document.getElementById("startBtn");
 var quizFormEl = document.getElementById("quizForm");
+var quizQuestion = document.getElementById("quizQuestion")
+var quizAnswerA = document.getElementById("quizAnswerA");
+var quizAnswerB = document.getElementById("quizAnswerB");
+var quizAnswerC = document.getElementById("quizAnswerC");
+var quizAnswerD = document.getElementById("quizAnswerD");
+
+
+// hide quiz form on page load
+window.onload = function() {
+    quizFormEl.style.display = "none";
+}
 
 var questions = [
     {q: "Which of the following is NOT a falsy value?", a: "\"\"", b: "null", c: "0", d: "bigInt"}
@@ -19,6 +30,8 @@ var timesUp = function() {
 // Start Quiz
  startBtnEl.addEventListener("click", function(){
         startBtnEl.style.display = "none";
+        quizFormEl.style.display = "block"
+        timer();
         // quiz timer function
 function timer() {
     var time = 80;
@@ -32,9 +45,20 @@ function timer() {
         }
 
     }, 1000)
+    quiz();
 }
     })
 
     function quiz() {
+        for (var i = 0; i < questions.length; i++) {
+            quizQuestion.textContent = questions[i].q;
+            for (var j = 0; j < questions.length; j++) {
+                quizAnswerA.textContent = questions[j].a;
+                quizAnswerB.textContent = questions[j].b;
+                quizAnswerC.textContent = questions[j].c;
+                quizAnswerD.textContent = questions[j].d;
+            }
+
+        }
 
     }
