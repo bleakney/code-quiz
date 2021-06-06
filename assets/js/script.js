36c179e3ea320a39c6110cd1bf11da664ce0efba
Bleakney Bissett <bleakneybissett@gmail.com>
2021-06-06 13:58:59 -0500

    add all quiz questions and answers into questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // global variable declarations 
 var highscoresEl = document.getElementById("highscoresBtn");
 var timerEl = document.getElementById("timer");
-var currentScoreEl = document.getElementById("currentScore")
+var scoreEl = document.getElementById("currentScore")
 var startBtnEl = document.getElementById("startBtn");
 var quizFormEl = document.getElementById("quizForm");
 var quizQuestion = document.getElementById("quizQuestion")
@@ -9,17 +9,35 @@ var quizAnswerA = document.getElementById("quizAnswerA");
 var quizAnswerB = document.getElementById("quizAnswerB");
 var quizAnswerC = document.getElementById("quizAnswerC");
 var quizAnswerD = document.getElementById("quizAnswerD");
-
+var quizAnswers = document.getElementsByClassName("quizAnswers");
+var score = 0;
+var time = 80;
 
 // hide quiz form on page load
 window.onload = function() {
     quizFormEl.style.display = "none";
+    scoreEl.style.display = "none";
+    timerEl.style.display = "none";
+
 }
 
+// QUESTIONS ARRAY
 var questions = [
-    {q: "Which of the following is NOT a falsy value?", a: "\"\"", b: "null", c: "0", d: "bigInt"}
-]
-console.log(questions[0]);
+    // first question
+    {question: "Which of the following is NOT a falsy value?", 
+    answers: { a: "\"\"", b: "null", c: "0", d: "bigInt"}, 
+    correctAnswer: "a"},
+    // second question
+    {question: "Where should you insert the <script> tag in the HTML document?", answers: {a: "<head>", b: "<main>", c: "<body>", d: "<section>"}, correctAnswer: "c"}, 
+    // third question
+    {question: "Which type of variable takes precedence over the other if names are the same?", answers: {a: "global variable", b: "local variable", c: "none of the above", d: "whichever variable is declared first"}, correctAnswer: "b"}, 
+    // fourth question
+    {question: "Which of the following methods removes the last element of an array?", answers: {a: ".push()", b: ".pop()", c: ".shift()", d: ".unshift()"}, correctAnswer: "b"},
+    // fifth question
+    {question: "To create a new repository in git, what should you type into the terminal?", answers: {a: "git start", b: "git clone <ssh key from github>", c: "git remote add <ssh key from github>", d: "git init"}, correctAnswer: "d"}
+];
+console.log(questions)
+
 
 
 // End Quiz
@@ -31,10 +49,12 @@ var timesUp = function() {
  startBtnEl.addEventListener("click", function(){
         startBtnEl.style.display = "none";
         quizFormEl.style.display = "block"
+        timerEl.style.display = "flex";
+        scoreEl.style.display = "flex";
+        scoreEl.textContent = "Score: " + score;
         timer();
         // quiz timer function
 function timer() {
-    var time = 80;
     var timeInterval = setInterval(function() {
         timerEl.textContent = time;
         if (time >= 1) {
@@ -43,22 +63,40 @@ function timer() {
             clearInterval(timeInterval);
             timesUp();
         }
+    var decrementTime = function() {
+        return time - 5;
+    }
 
     }, 1000)
-    quiz();
+    quizQuestions();
 }
     })
 
-    function quiz() {
+    function quizQuestions() {
         for (var i = 0; i < questions.length; i++) {
-            quizQuestion.textContent = questions[i].q;
-            for (var j = 0; j < questions.length; j++) {
-                quizAnswerA.textContent = questions[j].a;
-                quizAnswerB.textContent = questions[j].b;
-                quizAnswerC.textContent = questions[j].c;
-                quizAnswerD.textContent = questions[j].d;
-            }
+            quizQuestion.textContent = questions[i].question;
+            quizAnswerA.textContent = questions[i].answers.a;
+            quizAnswerB.textContent = questions[i].answers.b;
+            quizAnswerC.textContent = questions[i].answers.c;
+            quizAnswerD.textContent = questions[i].answers.d;
+            quizAnswerA.addEventListener(function(){
+                if (questions[i].correctAnswer = "a") {
+                    score
+                } else {
+                    decrementTime();
+                }
+            })
 
+           }
         }
 
-    }
+           // when user presses start button, box with quiz questions and answers appears
+           // for loop containing questions and answers needs to iterate once correct quiz answer is selected
+           // correct answers must be identifiable 
+           // 
+           
+           
+            // if (quizQuestion.textContent = questions[0].q && )
+
+            //     }
+        
